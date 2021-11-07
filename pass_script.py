4196075cdba8dc4503360bc061cf39b80dc91c72
Francesco Giordano <80858619+DomGiorda@users.noreply.github.com>
2021-11-06 21:46:02 -0300

    Add files via upload

---
+++ pass_script.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+alp = string.printable
+
+def pass_gen(alp):
+    pass_w = ""
+    long = int(input("Ingrese la longitud de su password: "))
+    for _ in range(long):
+        pass_w = pass_w + random.choice(alp)
+    print(pass_w)
+
+#------------main funtion---------------#
+
+def main():
+    pass_gen(alp)
+
+if __name__ == "__main__":
+    main()
+
